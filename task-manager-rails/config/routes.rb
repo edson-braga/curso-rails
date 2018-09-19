e56b7915a7fa420ec63974ffdc7a0e48d33692d7
Edson Marcos Teixeira Da Silva Braga <embraga@stefanini.com>
2018-09-19 14:51:46 -0300

    Criação da rota para a API

--- task-manager-rails/config/routes.rb
+++ task-manager-rails/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #namespace -> fornece a identificação da aplicação
+  #defaults -> especifica o tipo de formato no qual será utilzado para devolver a informação
+  #constraints -> especifica restrições, neste caso só entra se o subdominio for 'api' EX: www.site.com -> api.site.com
+  #path -> especifica o contexto da aplicação EX: api.site.com/task, ou api.site.com/backend/task - caso o valor de path seja backend
+  namespace :api, defaults: { format: :json }, constraints: { subdomain : 'api' }, path: "/" do
+
+  end
 end
